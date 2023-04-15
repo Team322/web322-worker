12385f7a02e4539dc37714815e616a606acb3f13
Alan Chang <tcode2k16@gmail.com>
2023-04-15 20:05:33 +0900

    6 chains

--- core/worker.js
+++ core/worker.js
@@ -8,6 +8,9 @@ const fs = require("fs");
 const path = require("path");
 const Web3 = require('web3');
 
+const config = require('../config.js');
+const deployment_info = require('../deployment.json');
+
 
 const getAbi = name => {
   try {
@@ -18,7 +21,7 @@ const getAbi = name => {
     const file = fs.readFileSync(dir, "utf8");
     const json = JSON.parse(file);
     const abi = json.abi;
-    console.log(`abi`, abi)
+    // console.log(`abi`, abi)
 
     return abi;
   } catch (e) {
@@ -35,18 +38,19 @@ const makeRequest = async (contract, url) => {
   // console.log(r);
 }
 
-async function main() {
-  const config = require('../config.js');
-  const deployment_info = require('../deployment.json');
-
-  const network = 'sepolia';
+async function montiorNetwork(network) {
+  function log(...args) {
+    console.log(`[${network}]`.padEnd(20, ' '), ...args);
+  }
   
+  log(`start monitoring network`);
+
   const web3 = new Web3(config[network].ws);
   const Contract = web3.eth.Contract;
 
   const account = web3.eth.accounts.privateKeyToAccount('0x'+config.account);
   web3.eth.accounts.wallet.add(account);
-  console.log(`web3.eth.defaultAccount: ${web3.eth.defaultAccount}`);
+  // log(`web3.eth.defaultAccount: ${web3.eth.defaultAccount}`);
 
 
   // Contract.setProvider(config.sepolia_ws);
@@ -59,25 +63,31 @@ async function main() {
   
   contract.events.Web2Request()
   .on("connected", async function(subscriptionId){
-    console.log(`subscriptionId: ${subscriptionId}`);
-    await makeRequest(contract, 'https://google.com/abc');
+    log(`subscriptionId: ${subscriptionId}`);
+    // await makeRequest(contract, 'https://google.com/abc');
   })
   .on('data', function(event){
-    console.log(event); // same results as the optional callback above
+    log(event); // same results as the optional callback above
     const sender = event.returnValues.sender;
     const id = event.returnValues.req.id;
     const req = Web3.utils.hexToAscii(event.returnValues.req.req);
-    console.log('Got a new event');
+    log('Got a new event');
     
-    console.log(`sender: ${sender}`);
-    console.log(`id: ${id}`);
-    console.log(`req: ${req}`);
+    log(`sender: ${sender}`);
+    log(`id: ${id}`);
+    log(`req: ${req}`);
     
 
   })
   .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-    console.log('error: '+error);
+    log('error: '+error);
   });  
+}
+
+async function main() {
+  for (const network of Object.keys(deployment_info)) {
+    montiorNetwork(network);
+  }
 }
 
 
